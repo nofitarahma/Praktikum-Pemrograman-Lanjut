b392bbe5db2b783c6521279c7d69a4df7597849a
nofitarahma <novikarahma542@gmail.com>
2025-03-04 20:02:20 +0700

    first commit

---
+++ PraktikumPemlan_modul1.java
@@ -0,0 +1,13 @@
+public class PraktikumPemlan_modul1 {
+    private String judulBuku;
+    private String kategoriBuku;
+    private String penulisBuku;
+    private int tahunTerbitBuku;
+
+    public void setJudulBuku(String judulBuku){
+        this.judulBuku = judulBuku;
+    }
+    public void setKategoriBuku(String kategoriBuku){
+        this.kategoriBuku = kategoriBuku;
+    }
+}
